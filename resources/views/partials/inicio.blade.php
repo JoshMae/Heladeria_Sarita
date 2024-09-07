4efa60fa6cdb58ccf3179321639bc3daf6db134d
JoshMP <emoratayah@miumg.edu.gt>
2024-09-06 22:41:20 -0600

    Mapa y Estilizado 100%

--- resources/views/partials/inicio.blade.php
+++ resources/views/partials/inicio.blade.php
@@ -1,27 +1,23 @@
 <!-- Barra Inferior (solo se muestra en la vista principal) -->
-<div class="footer-custom">
-    <div>
-        <span>
-            <a href="https://wa.me/1234567890" target="_blank">
-                <img src="{{ asset('images/whatsapp.png') }}" alt="WhatsApp" class="iconRedes" >
-                +502 2020 0000
+<footer class="footer-custom">
+    <div class="footer-content">
+        <div class="footer-left">
+            <a href="https://wa.me/1234567890" target="_blank" class="footer-link">
+                <img src="{{ asset('images/whatsapp.png') }}" alt="WhatsApp" class="icon-redes">
+                <span>+502 2020 0000</span>
             </a>
-        </span>
-        <span>
-            <a href="https://www.instagram.com/sarita" target="_blank">
-                <img src="{{ asset('images/instagram.png') }}" alt="Instagram" class="iconRedes">
-                @heladossarita
+            <a href="https://www.instagram.com/sarita" target="_blank" class="footer-link">
+                <img src="{{ asset('images/instagram.png') }}" alt="Instagram" class="icon-redes">
+                <span>@heladossarita</span>
             </a>
-        </span>
-        <span>
-            <a href="https://www.facebook.com/sarita" target="_blank">
-                <img src="{{ asset('images/facebook.png') }}" alt="Facebook" class="iconRedes">
-                Helados Sarita
+            <a href="https://www.facebook.com/sarita" target="_blank" class="footer-link">
+                <img src="{{ asset('images/facebook.png') }}" alt="Facebook" class="icon-redes">
+                <span>Helados Sarita</span>
             </a>
-        </span>
+        </div>
+        <div class="footer-right">
+            <span class="titulo-inferior">¡ Creamos momentos de alegría !</span>
+            <img src="{{ asset('images/icono.png') }}" alt="IconoSarita" class="icon-helado">
+        </div>
     </div>
-    <div id="Lema">
-        <span class="tituloInferior">¡ Creamos momentos de alegría !</span>
-        <img src="{{ asset('images/icono.png') }}" alt="IconoSarita" class="iconRedes" id="helado">
-    </div>
-</div>
+</footer>
